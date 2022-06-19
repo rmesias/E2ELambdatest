a9af7ddcb79d6fb4a9fac16fb1b3a6fc0458d1d5
Reyjie Mesias <mesiasreyj@gmail.com>
2022-06-19 20:21:57 +0800

    Update: Configure to Plugins to have cypress accept Cucumber gherkins syntax with typescript

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -30,11 +30,10 @@ module.exports = (on, config) => {
 const browserify = require("@cypress/browserify-preprocessor");
 
 module.exports = (on) => {
-  const options = browserify.defaultOptions;
+  const options = {
+    ...browserify.defaultOptions,
+    typescript: require.resolve("typescript"),
+  };
 
-  options.browserifyOptions.plugin.unshift(['tsify']);
-  // Or, if you need a custom tsconfig.json for your test files:
-  // options.browserifyOptions.plugin.unshift(['tsify', {project: 'path/to/other/tsconfig.json'}]);
-  
   on("file:preprocessor", cucumber(options));
 };
